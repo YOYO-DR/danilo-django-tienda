92717809ba100f4d6d884786bcb6608547454fa5
yoiner_duran <duranyoiner86@gmail.com>
2023-03-08 14:27:46 -0500

    commit 8/3

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,2 +1,9 @@
-from apps.home import index
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+  path('',index,name='index'),
+  path('f2/',home,name='home')
+]
 
--- apps/home/views.py
+++ apps/home/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>Hola, me encuetro en la aplicacion de Home.</h1>")
+    return HttpResponse("<h1 style='font-family:sans-serif; color: red;width: 100%;height:100%;display:flex;justify-content:center;align-items:center'>Hola Yoiner, me encuetro en la aplicacion de Home.</h1>")
+
+def home(request):
+    return HttpResponse("<h1 style='font-family:sans-serif; color: red;'>Hola Yoiner, Esto es una nueva funcion de home.</h1>")
